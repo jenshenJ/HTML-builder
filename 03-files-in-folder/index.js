b29941bd5e6b2ca7413bf3d08a737b006041fcab
jenshenJ <vip.carich@gmail.com>
2022-11-07 00:35:37 +0300

    feat: complete 03-files-in-folder task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,4 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.promises.readdir(path.join('03-files-in-folder', 'secret-folder'));
+const folderPath = path.join('03-files-in-folder', 'secret-folder');
+let folderInside = fs.readdirSync(folderPath);
+
+folderInside = folderInside.forEach((fileName) => {
+    let filePath = path.join(folderPath, fileName);
+    let fileStat = fs.statSync(filePath);
+    if(fileStat.isFile()){
+        console.log(`${fileName.split('.')[0]} - ${fileName.split('.')[1]} - ${fileStat.size / 1000}kb`);
+    }
+})
+
+
+
+
